9e1ea8c0340440cd40783402721184169c326cb8
ShimaaMohamed0 <118725184+ShimaaMohamed0@users.noreply.github.com>
2023-01-25 17:46:35 +0200

    add back ground to panel

--- EmployeeMgmt1/EmployeeMgmt1/login.Designer.cs
+++ EmployeeMgmt1/EmployeeMgmt1/login.Designer.cs
@@ -33,6 +33,7 @@
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
             this.panel1.Location = new System.Drawing.Point(51, 37);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(166, 94);
