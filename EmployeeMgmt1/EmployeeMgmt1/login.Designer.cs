f85d184e0e42cdbbdd23374e4aa7f8b755509566
ShimaaMohamed0 <118725184+ShimaaMohamed0@users.noreply.github.com>
2023-01-25 18:14:28 +0200

    add font to another label

--- EmployeeMgmt1/EmployeeMgmt1/login.Designer.cs
+++ EmployeeMgmt1/EmployeeMgmt1/login.Designer.cs
@@ -85,7 +85,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(311, 107);
+            this.label2.Location = new System.Drawing.Point(205, 107);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(64, 20);
             this.label2.TabIndex = 4;
