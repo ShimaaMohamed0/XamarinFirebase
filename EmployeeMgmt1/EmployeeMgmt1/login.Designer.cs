3f5bdd3b444dffc778a5e4de046f3fe91d494a55
ShimaaMohamed0 <118725184+ShimaaMohamed0@users.noreply.github.com>
2023-01-25 18:20:14 +0200

    add text to button

--- EmployeeMgmt1/EmployeeMgmt1/login.Designer.cs
+++ EmployeeMgmt1/EmployeeMgmt1/login.Designer.cs
@@ -106,11 +106,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(367, 236);
+            this.button1.Location = new System.Drawing.Point(229, 253);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(183, 29);
             this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
+            this.button1.Text = "LOGIN";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // login
