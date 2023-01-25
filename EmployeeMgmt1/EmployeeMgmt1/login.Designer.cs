defbb24be4768c3dc897adb1ffceae27d4743956
ShimaaMohamed0 <118725184+ShimaaMohamed0@users.noreply.github.com>
2023-01-25 17:58:07 +0200

    add text to label

--- EmployeeMgmt1/EmployeeMgmt1/login.Designer.cs
+++ EmployeeMgmt1/EmployeeMgmt1/login.Designer.cs
@@ -47,9 +47,9 @@
             this.label1.ForeColor = System.Drawing.Color.Teal;
             this.label1.Location = new System.Drawing.Point(288, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.Size = new System.Drawing.Size(131, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Employee M S";
             // 
             // login
             // 
